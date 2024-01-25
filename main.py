8a095566693f1ab6bcaad7d26c291ba57672d136
Jojed1 <jocaley@siue.com>
2024-01-25 13:21:38 -0600

    Original Python File

---
+++ main.py
@@ -0,0 +1,17 @@
+def main():
+    print("Welcome to a basic Python Program")
+    x = 4
+    y = 13
+    y = x + y
+    print(y)
+    
+    computerCourses = ["CS286", "CS325", "CS140", "CS150", "CS234", "CS111", "CS150"]
+    
+    for z in computerCourses:
+        if z == "CS150":
+            break
+        else:
+            print(z + "Course")
+    
+if __name__ == "__main__":
+    main()
