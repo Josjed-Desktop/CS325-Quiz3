fb6e7b32e5cf31839153596390f10af9fa4b59d1
Jojed1 <jocaley@siue.com>
2024-01-26 17:43:10 -0600

    Changes to line 8

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     y = x + y
     print(y)
     
-    computerCourses = ["CS286", "CS325", "CS140", "CS150", "CS234", "CS111", "CS150"]
+    computerCourses = ["CS325", "CS140", "CS234", "CS111", "CS150"]
     
     for z in computerCourses:
         if z == "CS150":
